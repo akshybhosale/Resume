60c180ddc5839406dc4705021dc47ec113ef5294
akshybhosale <akshaybhosale2810@gmail.com>
2024-07-15 23:41:37 +0530

    file added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div className="text-grape text-center py-7">
+            <p className='font-bold pb-2'>Crafted with ❤️ by Akshay Bhosale</p>
+            <a 
+            href="https://github.com/akshybhosale" 
+            target="_blank"
+            rel="noopener noreferrer" 
+            className="underline hover:opacity-70">View code on GitHub</a>
+        </div>
+    )
+}
+
+export default Footer;
